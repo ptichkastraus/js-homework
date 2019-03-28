0be8473b11ff9013618fb20c51e31121ab8d0aba
Polina <smilyfacegirl2105@gmail.com>
2019-03-28 14:40:49 +0300

    change

--- js/homework1.js
+++ js/homework1.js
@@ -1,13 +1,15 @@
-let height = 125, width = 215, height2 = 85;
+let height = 125, width = 115, height2 = 85;
 let area = 2 * (height * width + width * height2 + height * height2);
 console.log(area);
 
 
-res = (height >= width), res = (width <= height2) ? "высота больше" : "ширина больше" ? "ширина больше" : "высота2 больше"; // здесь я, кажется, не допоняла логику
+// res = (height >= width), res = (width <= height2) ? "высота больше" : "ширина больше" ? "ширина больше" : "высота2 больше"; // здесь я, кажется, не допоняла логику
+res = (height > width) ? "высота больше" : "ширина больше или они равны";
 console.log(res);
 
+
 let s = 1000, a = 15, b = 25;
-let emparea = s - (a * b);
+let emparea = s % (a * b);
 console.log(emparea);
 
 let oval = '15dm2', oval2 = '600sm2', ring;
@@ -19,5 +21,5 @@ console.log(ring);
 let m = 15.2, n = 7.4;
 m = Math.abs(10 - m);
 n = Math.abs(10 - n);
-res = (n > m) ? "m ближе к 10" : "n ближе к 10";
+res = (n > m) ? "m ближе к 10" : "n ближе к 10 или они равноудалены";
 console.log(res);
